f53e639e3a3958fa60d8138dbcf3b8066fdcc21d
David Michieli <d.m.michieli@gmail.com>
2020-04-03 15:10:54 +1100

    accept user input

--- tests/test_toy_robot.py
+++ tests/test_toy_robot.py
@@ -1,2 +1,16 @@
-def test_placeholder():
-    assert False
+import sys
+
+import pytest
+
+from toy_robot import get_cli_arguments
+
+
+def test_get_cli_arguments_returns_supplied_filename(monkeypatch):
+    monkeypatch.setattr(sys, "argv", ["toy_robot.py", "some_input.txt"])
+    assert get_cli_arguments() == "some_input.txt"
+
+
+def test_get_cli_arguments_causes_exit_when_missing_file(monkeypatch):
+    monkeypatch.setattr(sys, "argv", ["toy_robot.py"])
+    with pytest.raises(SystemExit):
+        get_cli_arguments()
--- toy_robot.py
+++ toy_robot.py
@@ -1,6 +1,19 @@
+import argparse
+
+
+def get_cli_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "input_file", help="name of input file", type=str
+    )
+    arguments = parser.parse_args()
+    input_file = arguments.input_file
+    return input_file
+
+
 def main():
-    print("This is not yet a toy robot.")
+    input_file = get_cli_arguments()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     main()
